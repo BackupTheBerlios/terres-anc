6f9af5b6ad5ab36e9efe3f8b04553881750168c7
pachilor <pachilor>
2005-02-09 20:38:04 +0000

    Addition of headers indicating copyright and licensing informations to every files.

--- game-engine/deplacement.php
+++ game-engine/deplacement.php
@@ -1,28 +1,25 @@
 <?php
+/*
+Engine of ``Terres-Anciennes", a web-based multiplayer RPG.
+Copyright 2004, 2005 Nicolas Hess / Choplair-network / Nova Devs.
+#$Id: deplacement.php,v 1.3 2005/02/09 20:38:04 pachilor Exp $
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 session_start();
-
-/***************************************************************************
-* *                              deplacement.php
-* *                            -------------------
-* *   begin     : Monday, January 31, 2005
-* *   copyright : www.terres-anciennes.com
-* *   email     : nicolas.hess@gmail.com, mortys_666@hotmail.com
-* *               pachilor@hotmail.com  , stephmouton@hotmail.com
-**
-* *   Version: index.php v 0.0.1
-* *
-* *
-* ***************************************************************************/
-
-/***************************************************************************
-* *
-* *
-* *
-* *
-* *
-* *
-* ***************************************************************************/
-
 
 if(!$_SESSION['infos']['pseudo'])
 {
